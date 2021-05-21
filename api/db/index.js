23b7e49f3eec88cd383ee6b60309a4b8fa32a15b
Mirza Učanbarlić <derek@pop-os.localdomain>
2021-05-21 22:16:46 +0200

    revert ssl for heroku

--- api/db/index.js
+++ api/db/index.js
@@ -6,7 +6,7 @@ console.log(isProduction);
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-    ssl: false,
+    ssl: isProduction,
 });
 
 module.exports = { pool }
