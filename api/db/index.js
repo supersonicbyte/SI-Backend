4cfaf48d15ffff67a90f8e1a75493134dd1829fd
Mirza Učanbarlić <derek@pop-os.localdomain>
2021-05-21 22:13:56 +0200

    fix ssl for heroku

--- api/db/index.js
+++ api/db/index.js
@@ -2,10 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const isProduction = process.env.NODE_ENV === 'production';
+console.log(isProduction);
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-    ssl: isProduction,
+    ssl: false,
 });
 
 module.exports = { pool }
