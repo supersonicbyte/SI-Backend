239fee13fc82a8c71e67ba6b496baa1c7963b176
Mirza Učanbarlić <derek@pop-os.localdomain>
2021-05-21 21:58:43 +0200

    setup PostreSQL with express

--- api/db/start.js
+++ api/db/start.js
@@ -1,1 +1,26 @@
+const db = require('./index');
 
+// runs on server start
+const tableExists = "SELECT * FROM TEST";
+const createTestTable = "CREATE TABLE TEST(NAME TEXT NOT NULL)";
+const insertTest = "INSERT INTO TEST(NAME) VALUES ($1)";
+
+module.exports.createDB = async function createDB() {
+    try {
+        const exist = await db.pool.query(tableExists);
+        // table already exists
+        if (exist) return;
+    } catch (error) {
+        console.log(error);
+    }
+    try {
+        const res = await db.pool.query(createTestTable);
+    } catch (error) {
+        console.log(error);
+    }
+    try {
+        const res1 = await db.pool.query(insertTest, ["hello"]);
+    } catch (error) {
+        console.log(err, res)
+    }
+}
--- server.js
+++ server.js
@@ -1,41 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const pool = require('./api/db/db');
+const db = require('./api/db/index.js');
+const DBStartHelper = require('./api/db/start');
+
+DBStartHelper.createDB();
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(cors());
 
 
-// runs on server start
-const createTestTable = "CREATE TABLE TEST(NAME TEXT NOT NULL)";
-const insertTest = "INSERT INTO TEST(NAME) VALUES (hello)";
-
-async function create() {
-    const res = await pool.query(createTestTable, (err, res) => {
-        console.log(err, res)
-        client.end()
-    });
-    const res1 = await pool.query(insertTest, (err, res) => {
-        console.log(err, res)
-        client.end()
-    })
-}
-
-create();
-
 app.get('/', (req, res) => { res.send("<h1>Up and running.</h1>"); });
 
-app.get('/test', (req, res) => {
-    pool.query('SELECT * FROM TEST')
-        .then(res => res.send(res.rows[0]))
-        .catch(e => console.error(e.stack))
+app.get('/test', async(req, res) => {
+    try {
+        const result = await db.pool.query('SELECT * FROM TEST');
+        res.send(result.rows[0])
+    } catch (error) {
+        console.log(error);
+        res.send("Error");
+    }
 });
 
 app.listen(3000, () => {
     console.log('Server uspješno pokrenut!')
 });
-
-// app.use('/user', userRoutes)
-// app.use('/auth', authRoutes)
